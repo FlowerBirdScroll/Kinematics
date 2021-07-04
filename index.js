eacfc1f588d3dee5899850d287f273e5ba324c55
FlowerBirdScroll <tinyflowerbird@gmail.com>
2021-07-04 21:37:45 +0800

    func1

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ window.addEventListener('load', function() {
     const g = document.getElementById('g')
     const coeff = document.getElementById('coeff')
     const path = document.getElementById('path')
-    const v0Unit1 = document.getElementById('v0Unit1')
-    const v0Unit2 = document.getElementById('v0Unit2')
 
     let valG = g.value
     let valCoeff = coeff.value
@@ -14,7 +12,7 @@ window.addEventListener('load', function() {
     let valV0Unit2 = 0
 
     CalcAndUpdate(  )
-    
+
     g.addEventListener('keyup', function( event ) {
         valG = event.target.value
         CalcAndUpdate( )
@@ -49,6 +47,8 @@ window.addEventListener('load', function() {
          valV0Unit2 = valV0Unit2.toFixed(2)
         console.log(valV0Unit1, valV0Unit2)
 
+        const v0Unit1 = document.getElementById('v0Unit1')
+        const v0Unit2 = document.getElementById('v0Unit2')
         v0Unit1.value = valV0Unit1
         v0Unit2.value = valV0Unit2
     }
